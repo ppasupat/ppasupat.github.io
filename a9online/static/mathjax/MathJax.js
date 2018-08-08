d79adffa54e3b6c2816cef98ea3bde181f25c1a3
Panupong Pasupat <ppasupat@stanford.edu>
2018-08-07 23:14:09 -0700

    Moved

--- a9online/static/mathjax/MathJax.js
+++
@@ -1,3 +0,0 @@
-var script = document.createElement('script');
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=https://ppasupat.github.io/a9online/static/mathjax/local.js";
-document.head.appendChild(script);
